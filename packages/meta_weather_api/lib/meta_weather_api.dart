88f188e6645bc09f39f2bea87375f34489613bc2
aldilanurhadiputra@gmail.com <aldilanurhadiputra@gmail.com>
2022-04-05 22:54:00 +0700

    add cubit

--- packages/meta_weather_api/lib/meta_weather_api.dart
+++ packages/meta_weather_api/lib/meta_weather_api.dart
@@ -2,9 +2,3 @@ library meta_weather_api;
 
 export 'src/models/models.dart';
 export 'src/meta_weather_api_client.dart';
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
